ff1127f1b921bf428a60d4eba97e44b60fc2bcc5
Niccolò Fontana <niccolo.fontana@vidiemme.it>
2023-01-19 22:28:40 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CustomTabView
+
+A description of this package.
---
+++ Tests/CustomTabViewTests/CustomTabViewTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CustomTabView
+
+final class CustomTabViewTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CustomTabView().text, "Hello, World!")
+    }
+}
